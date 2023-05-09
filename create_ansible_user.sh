7707798bdf58367a93f5cd21f8267c6782141799
martamazurkiewicz <marta@mazurkiewi.cz>
2023-05-09 22:01:56 +0200

    Fixing permissions

--- create_ansible_user.sh
+++ create_ansible_user.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 [[ $1 == */authorized_keys ]] || { echo "Provide authorized_keys file" ; exit ; }
-sudo useradd -m -s /sbin/nologin ansible
+sudo useradd -m -s /bin/bash ansible
 sudo usermod -aG sudo ansible
 [[ $(whoami) == root ]] || sudo usermod -aG ansible "$(whoami)"
 echo "ansible ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
-sudo install -m 640 -o ansible -g ansible -D -t /home/ansible/.ssh "$1"
+sudo install -m 600 -o ansible -g ansible -D -t /home/ansible/.ssh "$1"
 cat /home/ansible/.ssh/authorized_keys
