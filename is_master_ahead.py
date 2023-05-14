a5282e20d9f4a17ddc3ab972cb66cad6da865658
martamazurkiewicz <marta@mazurkiewi.cz>
2023-05-14 10:48:16 +0200

    No pull request vaiables yet

--- is_master_ahead.py
+++ is_master_ahead.py
@@ -11,11 +11,15 @@ organization = os.environ['SYSTEM_TEAMFOUNDATIONCOLLECTIONURI']
 print(organization)
 repo = os.environ['BUILD_REPOSITORY_NAME']
 print(repo)
-branch = os.environ['SYSTEM_PULLREQUEST_SOURCEBRANCH']
+#branch = os.environ['SYSTEM_PULLREQUEST_SOURCEBRANCH']
+branch = os.environ['BUILD_SOURCEBRANCH']
 print(branch)
 token = os.environ['SYSTEM_ACCESSTOKEN']
+print(token)
 headers = {"Authorization": "Bearer " + token}
+print(headers)
 commit_url = organization + project  + "/_apis/git/repositories/" + repo + "/commits?searchCriteria.itemVersion.version=master"
+print(commit_url)
 commit_response = requests.get(commit_url, headers=headers)
 print(commit_response)
 # latestcommitid = json.loads(comresponse)['value']['commitid'][0]
