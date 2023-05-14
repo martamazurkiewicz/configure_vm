f88af1dd3a6d620c11e385ebc1f2bcf4a94a50f6
martamazurkiewicz <marta@mazurkiewi.cz>
2023-05-14 10:44:32 +0200

    print all vars

--- is_master_ahead.py
+++ is_master_ahead.py
@@ -2,10 +2,17 @@ import json
 import os
 import requests
 
+print('ENV vars')
+print(os.environ)
+print('----')
 project = os.environ['SYSTEM_TEAMPROJECT']
+print(project)
 organization = os.environ['SYSTEM_TEAMFOUNDATIONCOLLECTIONURI']
+print(organization)
 repo = os.environ['BUILD_REPOSITORY_NAME']
+print(repo)
 branch = os.environ['SYSTEM_PULLREQUEST_SOURCEBRANCH']
+print(branch)
 token = os.environ['SYSTEM_ACCESSTOKEN']
 headers = {"Authorization": "Bearer " + token}
 commit_url = organization + project  + "/_apis/git/repositories/" + repo + "/commits?searchCriteria.itemVersion.version=master"
